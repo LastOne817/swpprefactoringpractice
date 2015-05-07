af75530e9ecb03210128bafb2e358a0236d00f47
LastOne817 <lastone817@naver.com>
2015-05-07 10:28:22 +0900

    Test of extracted methods

--- TestDateCalculator.rb
+++ TestDateCalculator.rb
@@ -13,4 +13,45 @@ class TestDateCalculator < Minitest::Test
         refute helper_leapyear(1900)
         assert helper_leapyear(2000)
     end
+
+    def help_addleapyear(days, year)
+        calc = DateCalculator.new(days)
+        calc.year = year
+        calc.add_leap_year
+        return calc.year
+    end
+
+    def test_addleapyear
+        assert_equal help_addleapyear(255, 2008), 2008
+        assert_equal help_addleapyear(400, 2008), 2009
+        assert_equal help_addleapyear(366, 2008), 2009
+    end
+
+    def help_addregularyear(days, year)
+        calc = DateCalculator.new(days)
+        calc.year = year
+        calc.add_regular_year
+        return calc.year
+    end
+
+    def test_addregularyear
+        assert_equal help_addregularyear(255, 2009), 2009
+        assert_equal help_addregularyear(400, 2009), 2010
+        assert_equal help_addregularyear(365, 2009), 2010
+    end
+
+    def help_convert(days, year)
+        calc = DateCalculator.new(days)
+        calc.year = year
+        return calc.convert
+    end
+
+    def test_convert
+        assert_equal help_convert(364, 2009), 2009
+        assert_equal help_convert(365, 2008), 2008
+        assert_equal help_convert(365, 2009), 2010
+        assert_equal help_convert(366, 2008), 2009
+        assert_equal help_convert(366, 2009), 2010
+        assert_equal help_convert(367, 2008), 2009
+    end
 end
